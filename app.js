3a84e01aa01b759773f984dd01f963f869f4fe40
anirudhagarwal73 <anirudhagarwal73@gmail.com>
2016-06-21 16:54:45 +0530

    Top Stories only ID

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express=require('express');
+var https=require('https');
 var app=express();
+var favicon = require('serve-favicon');
+
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/images/favicon1.ico'));
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+  res.sendFile( __dirname + "/views/index.html" );
 });
 
+app.get('/topstories', function (req, res) {
+  https.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty',function(response){
+    response.setEncoding('utf8');
+    response.on('error',console.error);
+    response.on('data',function(data){
+      res.send(data);
+    });
+  });
+});
+
+
 var server = app.listen(3000, function () {
-  console.log("Up and Running ........ ");
+  console.log('Up and Running ........ ');
 });
