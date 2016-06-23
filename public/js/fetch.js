af5a10f18b3231ca1b7c8f35e061e71dcd305a29
anirudhagarwal73 <anirudhagarwal73@gmail.com>
2016-06-23 21:02:34 +0530

    Improved Performance

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -3,23 +3,16 @@ var storiesCount=0;
 var storyNo=0;
 var pageno=0;
 var getURL='topstories';
+var functionQueue=[];
+var finished=1;
+var p1=0,p2;
 Date.now = function() { return new Date().getTime(); }
-
-function showModal(e,element){
-  e.preventDefault();
-  var url=element.children[0].innerHTML;
-  console.log(url);
-  var iframe='<iframe src="'+url+'" style="width: 100%; height:75%;"></iframe>';
-  $('.modal-body').html(iframe);
-  $('#myModal').modal('show');
-  return false;
-}
 
 function loadMore(e,element){
   e.preventDefault();
   $('#more').remove();
   pageno++;
-  batchLoad(30*pageno,30*pageno+30);
+  batchLoad(20*pageno,20*pageno+20);
 }
 
 function getTimeDiff(time){
@@ -48,16 +41,18 @@ function pagination(type){
 function batchLoad(s,end){
   if(s>=storiesCount){
     pagination(2);
+    finished=1;
     return;
   }
   if(s>=end){
     pagination(1);
+    finished=1;
     return;
   }
   var ids=[];
   console.log(s);
-  var e=s+6;
-  if(s+6>=storiesCount)
+  var e=s+5;
+  if(s+5>=storiesCount)
     e=storiesCount;
   for(i=s;i<e;i++)
     ids.push(storiesID[i]);
@@ -107,77 +102,69 @@ function batchLoad(s,end){
         htmlAddition+='<tr class="spacer"></tr>';
       });
       $('tbody').append(htmlAddition);
-      if(s==6){
+      if(s==5 && functionQueue.length==0){
         $( "#overlay1" ).fadeOut( "slow");
       }
-      batchLoad(s+6,end);
+      batchLoad(s+5,end);
     }
   });
 }
 
-function loadPage(){
+function load(t,e){
+  e.preventDefault();
+  p1=t;
+  p2=e;
+  $('#top').css({"background-color": "", "color": "#999999"});
+  $('#new').css({"background-color": "", "color": "#999999"});
+  $('#best').css({"background-color": "", "color": "#999999"});
+  $('#show').css({"background-color": "", "color": "#999999"});
+  $('#jobs').css({"background-color": "", "color": "#999999"});
+  $('#'+e.target.id).css({"background-color": "#fff", "color": "black"});
+  $('#content-table').empty();
+  $( '#more' ).remove();
   $( "#overlay1" ).fadeIn( "slow");
+  functionQueue.push(loadPage);
+}
+function loadPage(type,e){
+  if(type===1){
+    getURL='/'+e.target.id+'stories';
+    storyNo=0;
+  }
+  else{
+    $('#top').css({"background-color": "#fff", "color": "black"});
+    getURL='/topstories';
+  }
   $.get(getURL, function(data, status){
     if(status=="success")
     {
       storiesID=JSON.parse(data);
       storiesCount=storiesID.length;
       console.log(storiesCount);
-      batchLoad(0,30);
+      batchLoad(0,20);
     }
   });
 }
 $( document ).ready(function() {
-  loadPage();
-  
-  $("#top").click(function(e) {
-    e.preventDefault();
-    $('#top').css({"background-color": "#fff", "color": "black"});
-    $('#content-table').empty();
-    $( '#more' ).remove();
-    getURL='/topstories';
-    storyNo=0;
-    loadPage();
-  });
-  $("#new").click(function(e) {
-    e.preventDefault();
-    $('#new').css({"background-color": "#fff", "color": "black"});
-    $('#content-table').empty();
-    $( '#more' ).remove();
-    getURL='/newstories';
-    storyNo=0;
-    loadPage();
-  });
-  $("#best").click(function(e) {
-    e.preventDefault();
-    $('#best').css({"background-color": "#fff", "color": "black"});
-    $('#content-table').empty();
-    $( '#more' ).remove();
-    getURL='/beststories';
-    storyNo=0;
-    loadPage();
-  });
-  $("#show").click(function(e) {
-    e.preventDefault();
-    $('#show').css({"background-color": "#fff", "color": "black"});
-    $('#content-table').empty();
-    $( '#more' ).remove();
-    getURL='/showstories';
-    storyNo=0;
-    loadPage();
-  });
-  $("#jobs").click(function(e) {
-    e.preventDefault();
-    $('#jobs').css({"background-color": "#fff", "color": "black"});
-    $('#content-table').empty();
-    $( '#more' ).remove();
-    getURL='/jobsstories';
-    storyNo=0;
-    loadPage();
-  });
+  $("#menu-toggle").fadeOut("fast");
+  functionQueue.push(loadPage);
+  setInterval(function(){
+    if(finished==1){
+      if(functionQueue.length!=0){
+        finished=0;
+        (functionQueue.shift())(p1,p2);
+      }
+    }
+  }, 100);
   $("#menu-toggle").click(function(e) {
     e.preventDefault();
+    $("#menu-toggle").fadeOut("slow");
+    $("#menu-toggle1").fadeIn("fast");
     $("#wrapper").toggleClass("toggled");
-    loadPage();
+  });
+  $("#menu-toggle1").click(function(e) {
+    e.preventDefault();
+    $("#menu-toggle").fadeIn("slow");
+    $("#menu-toggle1").fadeOut("fast");
+    $("#wrapper").toggleClass("toggled");
   });
 });
